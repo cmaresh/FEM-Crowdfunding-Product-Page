5a525c85ff8d84053b409f246b19e3db22b367a2
Colin Maresh <cmaresh57@gmail.com>
2021-02-15 16:06:50 -0600

    markup added

---
+++ gulpfile.js
@@ -0,0 +1,24 @@
+const gulp = require('gulp');
+const gulp_sass = require('gulp-sass');
+const browser_sync = require('browser-sync').create();
+
+function sass() {
+  return gulp.src('./*.scss')
+    .pipe(gulp_sass())
+    .pipe(gulp.dest('.'))
+    .pipe(browser_sync.stream());
+}
+
+function sync() {
+  browser_sync.init({
+    server: {
+        baseDir: "./"
+    }
+  });
+}
+
+function watch() {
+  gulp.watch('./*.scss', { ignoreInitial: false }, sass);
+}
+
+exports.default = gulp.parallel(sync, watch);
